114952a200b995362b261881dfd53c56be2890fc
eryirawan <irawan_ery@yahoo.co.id>
2022-05-09 17:06:32 +0700

    add ssh log

--- sampling.sh
+++ sampling.sh
@@ -1,6 +1,6 @@
 Ini adalah tugas ke-4 SQL Shell tooling
 
-Tambahkan dataset dan download dengan wget 
+1. Tambahkan dataset dan download dengan wget 
 $ wget https://github.com/labusiam/dataset/raw/main/weather_data.xlsx
 
-
+2. [Todo] buat script convert ke database
