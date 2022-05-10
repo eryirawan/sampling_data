3f2807927cd165218b18c7fc437f25d50c97b71a
eryirawan <irawan_ery@yahoo.co.id>
2022-05-11 05:22:49 +0700

    convert xlsx to csv

--- sampling.sh
+++ sampling.sh
@@ -3,4 +3,6 @@ Ini adalah tugas ke-4 SQL Shell tooling
 1. Tambahkan dataset dan download dengan wget 
 $ wget https://github.com/labusiam/dataset/raw/main/weather_data.xlsx
 
-2. [Todo] buat script convert ke database
+2. Convert file weather_data.xlsx menjadi:
+  i. Sheet weather_2014 menjadi file weather_2014.csv
+  ii. Sheet weather_2015 menjadi file weather_2015.csv
