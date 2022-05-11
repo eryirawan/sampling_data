c7f0ce5538642218a6436928854e420c8369d854
eryirawan <irawan_ery@yahoo.co.id>
2022-05-11 09:25:05 +0700

    weather.csv

--- sampling.sh
+++ sampling.sh
@@ -6,3 +6,8 @@ $ wget https://github.com/labusiam/dataset/raw/main/weather_data.xlsx
 2. Convert file weather_data.xlsx menjadi:
   i. Sheet weather_2014 menjadi file weather_2014.csv
   ii. Sheet weather_2015 menjadi file weather_2015.csv
+
+3. Menggabungkan Data weather 2014 dan 2015 menjadi 1 csv kemudian diberi nama weather.csv. menghapus File weather_data.xlsx
+
+4. 
+
